fa2cd7a1144aa573ce7a2ae6e9f25765eb3df224
John Labovitz <johnl@johnlabovitz.com>
2023-08-14 10:24:55 -0400

    Fix test.

--- test/group_test.rb
+++ test/group_test.rb
@@ -3,7 +3,7 @@ require 'minitest/power_assert'
 
 require 'addressable-prettify'
 
-class Test < MiniTest::Test
+class Test < Minitest::Test
 
   def test_simple
     uri = Addressable::URI.parse('https://www.wsj.com/articles/in-the-rubble-of-ukraines-second-largest-city-survivors-make-their-stand-nobody-wants-the-russians-11647013623?st=jyhg95p9cgciy6f&reflink=share_mobilewebshare')
